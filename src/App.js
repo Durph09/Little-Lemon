07515aa726045b65d143b705206f1b530c033a0c
Conor Mahaney <crmahaney9@gmail.com>
2022-12-29 19:07:56 -0500

    updated app

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <>
+    <Header />
+    <Nav />
+    <Main />
+    <Footer />
       Home Page!
-    </div>
+    </>
   );
 }
 
