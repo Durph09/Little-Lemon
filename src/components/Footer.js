d2752b202796bef35e126c08edc4df7db2958873
Conor Mahaney <crmahaney9@gmail.com>
2023-01-02 10:53:06 -0500

    commit footer template

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,18 @@
 function Footer  () {
 
     return (
-        <ul>
-            <li><a href="#"></a></li>
-        </ul>
+        <footer>
+            {/* image*/}
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Menu</a></li>
+                <li><a href="#">Reservations</a></li>
+                <li><a href="#">Order Online</a></li>
+                <li><a href="#">Login</a></li>
+            </ul>
+            <address>123 Main Drive, Chicago IL.</address>
+        </footer>
     )
 
 }
