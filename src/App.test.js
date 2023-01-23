bd787e1221fa24f664fd8d8bce7dacf10f49333c
Conor Mahaney <crmahaney9@gmail.com>
2023-01-23 13:11:55 -0500

    first unit test created

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,31 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import BookingForm from './components/BookingForm';
+import {initializeTimes, updateTimes} from './App';
+
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+
+test('Renders the BookingForm heading',() => {
+  render(<BookingForm />);
+  const headingElement = screen.getByText("Book Now");
+  expenct(headingElement).toBeInTheDocument();
+})
+
+test('initializeTimes returns the correct value', () => {
+  const expectedValue = ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'];
+  expect(initializeTimes()).toEqual(expectedValue)
+  
+})
+
+
+
+test('updateTimes returns the same value provided in the state', () => {
+    const state = {date: '2022-01-01', availableTimes: ['17:00', '18:00', '19:00']};
+    expect(updateTimes(state)).toEqual(state.availableTimes);
 });
--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,60 +1,60 @@
-import e from "express";
-import { useState } from "react"
 
-const BookingForm = () => {
-
-    const [date, setDate] = useState("");
-    const [time, setTime] = useState("");
-    const [guests, setGuests] = useState("")
-    const [occasion, setOccasion]= useState("")
-
-    const [availableTimes, setAvailableTimes] = useState(['17:00','18:00', '19:00', '20:00', '21:00', '22:00']);
-
-    const handleChange= e => {
-        switch (e.target.id) {
-            case 'res-date':
-                setDate(e.target.value);
-                break;
-            case 'res-time':
-                setTime(e.target.value) ;
-                break;
-            case 'number':
-                setGuests(e.target.value);
-                break;
-            case 'occasion':
-                setOccasion(e.target.value);
-                break;
-            default:
-        }
-    }
-
-    const handleSubmit = () => console.log('submitted')
-
-    return(
-        <>
-        
-        <form style={{display: 'grid',justifyContent: 'center', maxWidth: '200px', gap: '20px'}} onSubmit={handleSubmit}>
-   <label htmlFo="res-date" >Choose date</label>
-   <input type="date" id="res-date" onChange={handleChange} />
-   <label htmlFo="res-time">Choose time</label>
-   <select id="res-time ">
-      <option>17:00</option>
-      <option>18:00</option>
-      <option>19:00</option>
-      <option>20:00</option>
-      <option>21:00</option>
-      <option>22:00</option>
-   </select>
-   <label htmlFor="guests">Number of guests</label>
-   <input type="number" placeholder="1" min="1" max="10" id="guests" />
-   <label htmlFor="occasion">Occasion</label>
-   <select id="occasion">
-      <option>Birthday</option>
-      <option>Anniversary</option>
-   </select>
-   <input type="submit" value="Make Your reservation" />
+const BookingForm = (props) => {
+    const { formState, handleInputChange, handleDateChange } = props;
+    const { availableTimes } = formState;
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      // handle form submission here
+      console.log({ formState });
+    };
+  
+    return (
+      <>
+        <div className="singleContainer">
+          <form className="form" onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="res-date">Choose date</label>
+              <input
+                className="inputFeilds"
+                type="date"
+                id="res-date"
+                name="date"
+                value={formState.date}
+                onChange={(e) => handleDateChange(e)}
+              />
+            </div>
+            <div>
+              <label htmlFor="res-time">Choose time</label>
+              <select
+                id="res-time "
+                name="time"
+                value={formState.time}
+                onChange={(e) => handleInputChange(e)}
+                className="inputFeilds"
+              >
+                {availableTimes.map((time) => (
+                  <option key={time}>{time}</option>
+                ))}
+              </select>
+            </div>
+  
+            <div>
+              <label htmlFor="guests">Number of guests</label>
+              <input
+                className="inputFeilds" type="number" placeholder="1" min="1" max="10" id="guests" name="guest" value={formState.guest} onChange={(e) => handleInputChange(e)} />
+            </div>
+   
+            <div>
+                <label htmlFor="occasion">Occasion</label>
+                <select id="occasion" className="inputFeilds" name="occasion" value={formState.occasion} onChange={(e) => handleInputChange(e)}>
+                    <option>Birthday</option>
+                    <option>Anniversary</option>
+                </select>
+            </div>
+            <button type="submit">Make Your reservation</button> 
 </form>
-
+</div>
         </>
     )
 }
